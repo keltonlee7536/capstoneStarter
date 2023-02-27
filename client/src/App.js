2be285c50ef570f105b985989876be0251106eef
Tyler Guerricabeitia <hunets4506@gmail.com>
2023-02-27 10:29:34 -0700

    more changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Login from './Login';
 import Courses from './Courses';
 import Admin from './Admin';
-import Home from './Home'
+import Home from './Home';
+import SignUp from './SignUp';
 import {  BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 import "./App.css";
@@ -23,6 +24,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <ul className="navbar-nav mr-auto">
       <li><Link to={'/'} className="nav-link"> Home </Link></li>
+        <li><Link to={'/SignUp'} className="nav-link"> Sign Up </Link></li>
         <li><Link to={'/Login'} className="nav-link"> Login </Link></li>
         <li><Link to={'/Courses'} className="nav-link">Courses</Link></li>
         <li><Link to={'/Admin'} className="nav-link">Admin</Link></li>
@@ -31,6 +33,7 @@ function App() {
       {/* <hr /> */}
       <Routes>
         <Route exact path='/' component={Home} />
+          <Route path='/SignUp' component={SignUp} />
           <Route exact path='/Login' component={Login} />
           <Route path='/Courses' component={Courses} />
           <Route path='/Admin' component={Admin} />
--- client/src/login.js
+++ client/src/login.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-        <div>
-          <h2>Login</h2>
-        </div>
+      <div className='loginOverall'>
+      <div className='loginContainer'>
+          <div className='loginHeader'>
+              <h1>Login</h1>
+          </div>
+          <div className='credentials'>
+              <label>Email:</label>
+              <input type="text"></input>
+              <label>Password:</label>
+              <input type="text"></input>
+              <button type="submit" href="/Home">Login</button>
+          </div>
+          <div className='signUp'>
+              <p>Don't have an account?<span href='/SignUp'> Sign Up</span></p>
+          </div>
+      </div>
+  </div>
     );
   }
 }
