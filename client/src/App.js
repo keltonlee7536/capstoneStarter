55ff387dd200d2109d0ba0e69b2ce5526e32df9f
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-02-27 10:10:29 -0700

    2.27 10:10am

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from './Login';
 import Courses from './Courses';
 import Admin from './Admin';
+import Home from './Home'
 import {  BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 import "./App.css";
@@ -19,17 +20,18 @@ function App() {
   return (
     <BrowserRouter>
     <div>
-      <h2>Welcome to React Router Tutorial {data} {rating}</h2>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <ul className="navbar-nav mr-auto">
-        <li><Link to={'/'} className="nav-link"> Login </Link></li>
+      <li><Link to={'/'} className="nav-link"> Home </Link></li>
+        <li><Link to={'/Login'} className="nav-link"> Login </Link></li>
         <li><Link to={'/Courses'} className="nav-link">Courses</Link></li>
         <li><Link to={'/Admin'} className="nav-link">Admin</Link></li>
       </ul>
       </nav>
       {/* <hr /> */}
       <Routes>
-          <Route exact path='/' component={Login} />
+        <Route exact path='/' component={Home} />
+          <Route exact path='/Login' component={Login} />
           <Route path='/Courses' component={Courses} />
           <Route path='/Admin' component={Admin} />
       </Routes>
